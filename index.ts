47e3ae718f066118d28665733445e953848a436d
Dre <dreomoj@gmail.com>
2023-08-07 15:42:37 +0100

    completed

---
+++ index.ts
@@ -0,0 +1,24 @@
+// var age: number
+// age = false
+
+// var firstName: string
+// var lastName: string
+// var phoneNumber: number
+// var married: boolean
+// var accountNumber: undefined
+// var disabilites: null
+
+
+// firstName = "Dre"
+// lastName = "Omoj"
+// phoneNumber = 8026855123
+// married = false
+// accountNumber = undefined
+// disabilites = null
+
+var firstName = "Dre"
+var lastName = "Omoj"
+var phoneNumber = 8026855123
+var married = false
+var accountNumber = undefined
+disabilites = null
